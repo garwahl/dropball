bc450c0fb989a75dd0fa074d00073420ed037404
garwahl <garwahlam@gmail.com>
2016-07-30 12:49:28 +1000

    Added simple createCharacter() + Fullscreen

--- public/js/game.js
+++ public/js/game.js
@@ -5,15 +5,29 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 // # Functions#
 // ############
 
-function createPlayer(player) {
-	
+// Create a new instance of the player at x,y coordinates using sprite player
+function createPlayer(x , y, player) {
+	var newPlayer;
+	newPlayer = game.add.sprite(x, y, player);
+
+	return newPlayer;
+}
+// Change the game into fullscreen
+function fullScreen() {
+	game.scale.pageAlignHorizontally = true;
+	game.scale.pageAlignVertically = true;
+	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	game.scale.setScreenSize(true);
+
 }
 
+// #############
 
 function preload() {
 }
 
 function create() {
+	fullScreen();
 }
 
 function update() {
