08e78580366d9783b01e095676abde16427f084e
edwaarrdd <edwaarrdd@gmail.com>
2016-07-30 18:58:52 +1000

    making changes

--- public/js/data.js
+++ public/js/data.js
@@ -16,16 +16,14 @@ window.onload = function() {
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 1,
             data: [0, 0, 0, 0, 0],
@@ -49,7 +47,6 @@ window.onload = function() {
 		datasets: [{
 		    label: '# of deaths',
 		    data: [1],
-		    /*
 		    fill: false,
             lineTension: 0.1,
             backgroundColor: "rgba(75,192,192,0.4)",
@@ -69,7 +66,6 @@ window.onload = function() {
             pointHitRadius: 10,
             data: [65, 59, 80, 81, 56, 55, 40],
             spanGaps: false,
-            */
 		}]
 	    },
 	    options = {
@@ -101,7 +97,6 @@ window.onload = function() {
 
 		}
 	});
-	/*
 	// Team composition pie chart
 	var teamcompGraphCanvas = document.getElementById('teamcompGraph');
 		type = 'pie',
@@ -133,8 +128,6 @@ window.onload = function() {
 		teamcompGraph.datasets[0].bars[distancetobargraph].value += 1;
 		//console.log(distanceGraph.datasets);
 		distanceGraph.update();
-		
+		*/
 	});
-	*/
-
 }
