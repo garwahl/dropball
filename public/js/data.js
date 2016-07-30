068f6bac908adf87d80b5c353f9fcee5236bae6d
edwaarrdd <edwaarrdd@gmail.com>
2016-07-30 21:00:09 +1000

    now on 2.0 properly lol

--- public/js/data.js
+++ public/js/data.js
@@ -3,44 +3,7 @@ var randomgraph;
 window.onload = function() {
 	socket = io();
 	console.log(socket);
-	var ctx = document.getElementById("myChart");
-	var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-	/*
+
 	var distanceGraphCanvas = document.getElementById("distanceGraph");
 	var distanceGraph = new Chart(distanceGraphCanvas, {
 	    type = 'bar',
@@ -72,7 +35,7 @@ window.onload = function() {
 		distanceGraph.datasets[0].bars[distancetobargraph].value += 1;
 		//console.log(distanceGraph.datasets);
 		distanceGraph.update();
-	});*/
+	});
 	//Distance travelled 
 	/*var distanceGraphCanvas = document.getElementById("distanceGraph");
 	var distanceGraph = new Chart(distanceGraphCanvas, {
