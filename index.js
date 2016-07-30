3f158f5c37b3633e5591ae093b85e20d2cb6e0ed
Kevin Imvoo <imvooz@gmail.com>
2016-07-30 12:47:02 +1000

    Add simple http server, rest of dependencies.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-var io = require('socket.io')(80);
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+	res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
