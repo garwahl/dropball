77b6f3ef6fe2a37321c0fcd9ed99dd0820c51c08
garwahl <garwahlam@gmail.com>
2016-07-30 12:18:59 +1000

    added html boiler, js boiler, phaser code

---
+++ public/js/game.js
@@ -0,0 +1,11 @@
+// Clientside scripts
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
