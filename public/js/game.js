618130928f8a76377d073b803140295d9f35149a
garwahl <garwahlam@gmail.com>
2016-07-30 15:41:51 +1000

    movement

--- public/js/game.js
+++ public/js/game.js
@@ -17,8 +17,8 @@ function createPlayer(sprite) {
 // Set the movement of each individual player variable passed as args
 function setMovement(player) {
 	game.physics.arcade.enable(player);
-	game.physics.enable(player, Phaser.Physics.Arcade);
 	
+
 	// Player collisions
 	player.body.collideWorldBounds = true;
 	player.body.bounce = 0.5;
